9fa6f5cff99ac28cbcb3d9c2cdc55d56c094813e
Grizli <neghmc@live.com>
2021-04-12 23:22:14 +0200

    Klasyfikacja i klasteryzacja

---
+++ cloud.py
@@ -0,0 +1,131 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from scipy.stats import truncnorm, uniform
+from csv import writer
+from math import pi
+from numpy import sin, cos, full, array, cross, dot, sqrt, ndarray, empty, seterr
+from numpy.random import default_rng
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+
+def surface_point(offset=(0, 0, 0), orientation="vert", num_points=2000, size=(10, 10)):
+    width, height = size
+    a = uniform(0, width).rvs(size=num_points)
+    b = uniform(0, height).rvs(size=num_points)
+    if orientation == "vert":
+        return zip(a + full(num_points, offset[0]), b + full(num_points, offset[1]), full(num_points, offset[2]))
+    elif orientation == "hor":
+        return zip(full(num_points, offset[0]), a + full(num_points, offset[1]), b + full(num_points, offset[2]))
+    else:
+        return None
+
+
+def cylinder_points(offset=(0, 0, 0), num_points=2000, radius=10, height=20):
+    radius = uniform(0, radius).rvs(size=num_points)
+    theta = uniform(0, 2 * pi).rvs(size=num_points)
+
+    cyl_mean = 0
+    cyl_std = 5
+    z = truncnorm(a=(0 - cyl_mean) / cyl_std, b=(height - cyl_mean) / cyl_std, loc=cyl_mean, scale=cyl_std) \
+        .rvs(size=num_points)
+
+    return zip(radius * cos(theta) + full(num_points, offset[0]), radius * sin(theta) + full(num_points, offset[1]),
+               z + full(num_points, offset[2]))
+
+
+# Own implementation of RANSAC algorithm
+# https://www.youtube.com/watch?v=9D5rrtCC_E0
+# https://en.wikipedia.org/wiki/Random_sample_consensus
+def my_ransac(data, max_iter=100):
+    # returns vector of a plane passing through 3 given points
+    def get_plane_vector(points):
+        p = points[1] - points[0]
+        q = points[2] - points[0]
+        # math
+        ABC = cross(p, q)
+        D = -dot(points[0], ABC)
+        return array([*ABC, D])
+
+    # returns the shortest distance from a point to a plane
+    def get_distance_from_plane(plane_vector, point):
+        ABC = plane_vector[0:3]
+        D = plane_vector[3]
+        # and more math
+        distance = abs(dot(ABC, point) + D) / sqrt(dot(ABC, ABC))
+        return distance
+
+    best_plane = None
+    best_score = 0
+
+    # changing to numpy type if needed
+    if type(data) is not ndarray:
+        data = array(data)
+
+    for iter in range(0, max_iter):
+        # selecting 3 different random points to create a plane
+        selected_plane = get_plane_vector([data[i] for i in default_rng().choice(len(data), size=3, replace=False)])
+        # counting number of points being close to the surfacez
+        model_score = sum([True for point in data if get_distance_from_plane(selected_plane, point) < 3])
+        # checking how it did
+        if model_score > best_score:
+            best_score = model_score
+            best_plane = selected_plane
+
+    # back to list
+    if best_plane is not None:
+        best_plane = best_plane.tolist()
+
+    return best_plane
+
+
+def generate_cloud():
+    # generating cloud points
+    cloud_points = [
+        *surface_point(offset=(10, 20, 10), orientation="vert", num_points=5000),
+        *surface_point(offset=(-10, -20, -10), orientation="hor", num_points=5000),
+        *cylinder_points(num_points=5000)
+    ]
+    with open('cloud.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+        csvwriter = writer(csvfile)
+        for p in cloud_points:
+            csvwriter.writerow(p)
+
+    # separating clouds to clusters
+    data = array(cloud_points)
+    prediction = KMeans(n_clusters=3).fit_predict(data)
+
+    red_shape = data[prediction == 0]
+    green_shape = data[prediction == 1]
+    blue_shape = data[prediction == 2]
+
+    # displaying them in 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(121, projection='3d')
+    ax.set_xlim([-20, 30])
+    ax.set_ylim([-30, 40])
+    ax.set_zlim([-20, 30])
+    ax.set_title("The cloud")
+
+    ax.scatter(*list(zip(*red_shape)), c='r')
+    ax.scatter(*list(zip(*green_shape)), c='g')
+    ax.scatter(*list(zip(*blue_shape)), c='b')
+
+    # finding best fitting planes and displaying vectors of the planes
+    ax = fig.add_subplot(122, projection='3d')
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
+    ax.set_zlim([-1, 1])
+    ax.set_title("Normalized vectors of the planes")
+
+    surf = my_ransac(red_shape, 300)
+    ax.quiver(0, 0, 0, *surf[0:3], pivot='tail', color='r', normalize=True)
+    surf = my_ransac(green_shape, 300)
+    ax.quiver(0, 0, 0, *surf[0:3], pivot='tail', color='g', normalize=True)
+    surf = my_ransac(blue_shape, 300)
+    ax.quiver(0, 0, 0, *surf[0:3], pivot='tail', color='b', normalize=True)
+
+    plt.show()
--- main.py
+++ main.py
@@ -1,133 +1,101 @@
-# This is a sample Python script.
+from PIL import Image
+from skimage.feature import greycomatrix, greycoprops
+from pandas import DataFrame
+from itertools import product
+import numpy as np
+from os.path import exists, dirname
+from os import mkdir
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from scipy.stats import truncnorm, uniform
-from csv import writer
-from math import pi
-from numpy import sin, cos, full, array, cross, dot, sqrt, ndarray, empty, seterr
-from numpy.random import default_rng
-from sklearn.cluster import KMeans
+# for classification
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix
+from sklearn.decomposition import PCA
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def surface_point(offset=(0, 0, 0), orientation="vert", num_points=2000, size=(10, 10)):
-    width, height = size
-    a = uniform(0, width).rvs(size=num_points)
-    b = uniform(0, height).rvs(size=num_points)
-    if orientation == "vert":
-        return zip(a + full(num_points, offset[0]), b + full(num_points, offset[1]), full(num_points, offset[2]))
-    elif orientation == "hor":
-        return zip(full(num_points, offset[0]), a + full(num_points, offset[1]), b + full(num_points, offset[2]))
-    else:
-        return None
+def generate_textures_features(textures_folder="."):
+    feature_names = ('dissimilarity', 'contrast', 'correlation', 'energy', 'homogeneity', 'ASM')  # cechy tekstur
 
+    distances = (1, 3, 5)
+    angles = (0, np.pi / 4, np.pi / 2, 3 * np.pi / 4)  # odległości i kąty dla jakich mają być tworzone macierze
 
-def cylinder_points(offset=(0, 0, 0), num_points=2000, radius=10, height=20):
-    radius = uniform(0, radius).rvs(size=num_points)
-    theta = uniform(0, 2 * pi).rvs(size=num_points)
+    def get_full_names():
+        dist_str = ('1', '2', '5')
+        angles_str = '0deg, 45deg, 90deg, 135deg'.split(',')
+        return ['_'.join(f) for f in product(feature_names, dist_str, angles_str)]
 
-    cyl_mean = 0
-    cyl_std = 5
-    z = truncnorm(a=(0 - cyl_mean) / cyl_std, b=(height - cyl_mean) / cyl_std, loc=cyl_mean, scale=cyl_std) \
-        .rvs(size=num_points)
+    def get_glcm_feature_array(patch):
+        patch_64 = (patch / np.max(patch) * 63).astype('uint8')  # dyskretyzacja obrazu do 64 poziomów jasności
+        glcm = greycomatrix(patch_64, distances, angles, 64, True, True)
+        feature_vector = []
+        for feature in feature_names:
+            feature_vector.extend(list(greycoprops(glcm, feature).flatten()))
+        return feature_vector
 
-    return zip(radius * cos(theta) + full(num_points, offset[0]), radius * sin(theta) + full(num_points, offset[1]),
-               z + full(num_points, offset[2]))
+    categories = ["wall", "door", "floor"]
+    mkdir(textures_folder + "\\cropped")
 
+    size = 128, 128  # rozmiar próbek
 
-# Own implementation of RANSAC algorithm
-# https://www.youtube.com/watch?v=9D5rrtCC_E0
-# https://en.wikipedia.org/wiki/Random_sample_consensus
-def my_ransac(data, max_iter=100):
-    # returns vector of a plane passing through 3 given points
-    def get_plane_vector(points):
-        p = points[1] - points[0]
-        q = points[2] - points[0]
-        # math
-        ABC = cross(p, q)
-        D = -dot(points[0], ABC)
-        return array([*ABC, D])
+    features = []
+    for category in categories:
+        img = Image.open(textures_folder + "\\" + category + ".jpg")
+        xr = np.random.randint(0, img.width - size[0], 10)
+        yr = np.random.randint(0, img.height - size[1], 10)  # losowanie 10ciu położeń próbek ze zdjęcia
+        for i, (x, y) in enumerate(zip(xr, yr)):  # enumerate() dodaje indeks
+            img_sample = img.crop((x, y, x + size[0], y + size[1]))
+            img_sample.save(textures_folder + "\\cropped\\" + category + f"{i:02d}" + ".jpg")
+            img_grey = img.convert('L')  # konwersja do skali szarości
+            feature_vector = get_glcm_feature_array(np.array(img_grey))  # generowanie cech tekstury
+            feature_vector.append(category)
+            features.append(feature_vector)
 
-    # returns the shortest distance from a point to a plane
-    def get_distance_from_plane(plane_vector, point):
-        ABC = plane_vector[0:3]
-        D = plane_vector[3]
-        # and more math
-        distance = abs(dot(ABC, point) + D) / sqrt(dot(ABC, ABC))
-        return distance
+    full_feature_names = get_full_names()
+    full_feature_names.append('Category')
 
-    best_plane = None
-    best_score = 0
-
-    # changing to numpy type if needed
-    if type(data) is not ndarray:
-        data = array(data)
-
-    for iter in range(0, max_iter):
-        # selecting 3 different random points to create a plane
-        selected_plane = get_plane_vector([data[i] for i in default_rng().choice(len(data), size=3, replace=False)])
-        # counting number of points being close to the surface
-        model_score = sum([True for point in data if get_distance_from_plane(selected_plane, point) < 3])
-        # checking how it did
-        if model_score > best_score:
-            best_score = model_score
-            best_plane = selected_plane
-
-    # back to list
-    if best_plane is not None:
-        best_plane = best_plane.tolist()
-
-    return best_plane
+    df = DataFrame(data=features, columns=full_feature_names)
+    df.to_csv('textures_data.csv', sep=',', index=False)
 
 
 def main():
-    # generating cloud points
-    cloud_points = [
-        *surface_point(offset=(10, 20, 10), orientation="vert", num_points=5000),
-        *surface_point(offset=(-10, -20, -10), orientation="hor", num_points=5000),
-        *cylinder_points(num_points=5000)
-    ]
-    with open('cloud.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
-        csvwriter = writer(csvfile)
-        for p in cloud_points:
-            csvwriter.writerow(p)
+    if not exists(r".\textures\cropped"):
+        generate_textures_features(r".\textures")
 
-    # separating clouds to clusters
-    data = array(cloud_points)
-    prediction = KMeans(n_clusters=3).fit_predict(data)
+    features = pd.read_csv('textures_data.csv', sep=',')
 
-    red_shape = data[prediction == 0]
-    green_shape = data[prediction == 1]
-    blue_shape = data[prediction == 2]
+    data = np.array(features)
+    x = (data[:, :-1]).astype('float64')
+    y = data[:, -1]
 
-    # displaying them in 3D
+    x_transform = PCA(n_components=3)
+    xt = x_transform.fit_transform(x)
+
+    red = y == 'wall'
+    blue = y == 'door'
+    green = y == 'floor'
+
     fig = plt.figure()
-    ax = fig.add_subplot(121, projection='3d')
-    ax.set_xlim([-20, 30])
-    ax.set_ylim([-30, 40])
-    ax.set_zlim([-20, 30])
-    ax.set_title("The cloud")
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(xt[red, 0], xt[red, 1], xt[red, 2], c="r")
+    ax.scatter(xt[blue, 0], xt[blue, 1], xt[blue, 2], c="b")
+    ax.scatter(xt[green, 0], xt[green, 1], xt[green, 2], c="g")
 
-    ax.scatter(*list(zip(*red_shape)), c='r')
-    ax.scatter(*list(zip(*green_shape)), c='g')
-    ax.scatter(*list(zip(*blue_shape)), c='b')
+    classifier = svm.SVC(gamma='auto')
 
-    # finding best fitting planes and displaying vectors of the planes
-    ax = fig.add_subplot(122, projection='3d')
-    ax.set_xlim([-1, 1])
-    ax.set_ylim([-1, 1])
-    ax.set_zlim([-1, 1])
-    ax.set_title("Normalized vectors of the planes")
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
 
-    surf = my_ransac(red_shape, 300)
-    ax.quiver(0, 0, 0, *surf[0:3], pivot='tail', color='r', normalize=True)
-    surf = my_ransac(green_shape, 300)
-    ax.quiver(0, 0, 0, *surf[0:3], pivot='tail', color='g', normalize=True)
-    surf = my_ransac(blue_shape, 300)
-    ax.quiver(0, 0, 0, *surf[0:3], pivot='tail', color='b', normalize=True)
+    classifier.fit(x_train, y_train)
+    y_pred = classifier.predict(x_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(acc)
 
+    cm = confusion_matrix(y_test, y_pred, normalize='true')
+
+    print(cm)
+
+    disp = plot_confusion_matrix(classifier, x_test, y_test, cmap=plt.cm.Blues)
     plt.show()
 
 
